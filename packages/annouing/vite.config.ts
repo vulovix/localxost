0fd1018fd685ef621d197ae208ba02594a760716
IvanVulovic <ivn.vulovic@gmail.com>
2023-08-09 14:20:11 +0200

    version update

--- packages/annouing/vite.config.ts
+++ packages/annouing/vite.config.ts
@@ -1,31 +1,32 @@
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        sourcemap: true,
-        lib: {
-            entry: path.resolve(__dirname, 'src/lib/index.ts'),
-            name: 'MyLib',
-            formats: ['es', 'umd'],
-            fileName: (format) => `annouing.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM'
-                },
-            },
-        },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "MyLib",
+      formats: ["es", "umd"],
+      fileName: (format) => `annouing.${format}.js`,
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
--- packages/cryptograph/src/index.ts
+++ packages/cryptograph/src/index.ts
@@ -2,70 +2,73 @@ import Subset from "./subset";
 import { SubsetEnum } from "./types";
 
 export function Cryptograph() {
-    let sentence = "";
-    let decrypted = "";
-    let placeholders = Subset.en;
+  let sentence = "";
+  let decrypted = "";
+  let placeholders = Subset.en;
 
-    function encrypt() {
-        const o = sentence.split("");
+  function encrypt() {
+    const o = sentence.split("");
 
-        for (let i = 0; i < o.length; i++) {
-            if (placeholders.includes(o[i])) {
-                o[i] = placeholders[Math.floor(Math.random() * placeholders.length)];
-            }
-        }
-        return o.join("");
+    for (let i = 0; i < o.length; i++) {
+      if (placeholders.includes(o[i])) {
+        o[i] = placeholders[Math.floor(Math.random() * placeholders.length)];
+      }
     }
+    return o.join("");
+  }
 
-    function decrypt() {
-        const enc = decrypted.split("").filter((x) => !!x);
-        if (decrypted.length !== sentence.length) {
-            decrypted = encrypt();
-        }
-        for (let i = 0; i < sentence.length; i++) {
-            if (enc[i] !== sentence[i]) {
-                const r = placeholders[Math.floor(Math.random() * placeholders.length)];
-                enc[i] = r;
-                const newSentence = enc.join("");
-                if (newSentence.length === sentence.length) {
-                    decrypted = newSentence;
-                }
-            }
-        }
-        return decrypted;
+  function decrypt() {
+    const enc = decrypted.split("").filter((x) => !!x);
+    if (decrypted.length !== sentence.length) {
+      decrypted = encrypt();
     }
-
-    const run = (s: string, speed = 20, cb = (d: string) => undefined) => {
-        sentence = s;
-        decrypted = encrypt();
-        const interval = setInterval(() => {
-            const isDone = decrypted === sentence;
-            decrypted = decrypt();
-            cb(decrypted);
-            if (isDone) {
-                clearInterval(interval);
-            }
-        }, speed);
-    };
-
-    function setSubset(subset: SubsetEnum) {
-        if (Subset[subset]) {
-            placeholders = Subset[subset];
+    for (let i = 0; i < sentence.length; i++) {
+      if (enc[i] !== sentence[i]) {
+        const r = placeholders[Math.floor(Math.random() * placeholders.length)];
+        enc[i] = r;
+        const newSentence = enc.join("");
+        if (newSentence.length === sentence.length) {
+          decrypted = newSentence;
         }
+      }
     }
-    function addSubset(subset: SubsetEnum) {
-        if (Subset[subset]) {
-            placeholders = [...placeholders, ...Subset[subset]];
-        }
-    }
+    return decrypted;
+  }
 
-    return {
-        run,
-        sentence,
-        encrypt,
-        decrypt,
-        decrypted,
-        setSubset,
-        addSubset,
-    };
+  const run = (s: string, speed = 20, cb = (d: string) => undefined) => {
+    sentence = s;
+    decrypted = encrypt();
+    const interval = setInterval(() => {
+      const isDone = decrypted === sentence;
+      decrypted = decrypt();
+      cb(decrypted);
+      if (isDone) {
+        clearInterval(interval);
+      }
+    }, speed);
+  };
+
+  function setSubset(subset: SubsetEnum) {
+    if (Subset[subset]) {
+      placeholders = Subset[subset];
+    }
+  }
+  function addSubset(subset: SubsetEnum) {
+    if (Subset[subset]) {
+      placeholders = [...placeholders, ...Subset[subset]];
+    }
+  }
+
+  return {
+    run,
+    sentence,
+    encrypt,
+    decrypt,
+    decrypted,
+    setSubset,
+    addSubset,
+  };
 }
+
+export * from "./types";
+export { default as Subset } from "./subset";
